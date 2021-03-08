8d8416d6e1e957c3140b7f85b5f43ff606c188fa
DieL89 <dzmitry273@gmail.com>
2021-03-08 16:37:50 +0300

    refactor: change replaceAll with replace

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ module.exports = function check(str, bracketsConfig) {
 
   while (true) {
     for (let i = 0; i < bracketsPatterns.length; i++) {
-      str = str.replaceAll(bracketsPatterns[i], '');
+      str = str.replace(bracketsPatterns[i], '');
     }
 
     if (str.length == 0) return true;
     if (str == controlStr) return false;
 
     controlStr = str;
-	}
+  }
 }
