04ff7dee3c2bf11c49c9928fcf96c191922eaf59
DieL89 <dzmitry273@gmail.com>
2021-03-08 16:28:20 +0300

    fix: change tabs and spaces

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-	if (str.length % 2) return false;
-	
-	let bracketsPatterns = bracketsConfig.map(array => array[0] + array[1]);
+  if (str.length % 2) return false;
+
+  let bracketsPatterns = bracketsConfig.map(array => array[0] + array[1]);
   let controlStr = str;
-	
-	while (true) {
-		for (let i = 0; i < bracketsPatterns.length; i++) {
-			str = str.replaceAll(bracketsPatterns[i], '');
-		}
-		
-		if (str.length == 0) return true;
+
+  while (true) {
+    for (let i = 0; i < bracketsPatterns.length; i++) {
+      str = str.replaceAll(bracketsPatterns[i], '');
+    }
+
+    if (str.length == 0) return true;
     if (str == controlStr) return false;
 
     controlStr = str;
